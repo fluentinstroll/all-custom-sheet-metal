d2a825e74ed0928120bed2ef1319dfc2355ec57c
fluentinstroll <fluentinstroll@protonmail.com>
2021-06-03 13:56:39 -0400

    added text and shadow to navbar brand

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import NavBar from '../components/NavBar';
 import Image from 'next/image';
-import styles from './Home.module.css';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
@@ -20,11 +20,14 @@ export default function Home() {
         alt="temp"
       />
       </div>
+      <div className={styles.headingText}>
       <p className={styles.bgText}>
         All Custom
-        <br />
+        </p>
+        <p className={styles.bgText}>
         Sheet Metal
       </p>
+      </div>
       </main>
 
       <footer>
