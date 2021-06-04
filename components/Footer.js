588ca3571b462036748b8335c6a3c988ccf1fe7b
fluentinstroll <fluentinstroll@protonmail.com>
2021-06-04 16:00:32 -0400

    added routes for various pages and components, and added boilerplate for those pages

---
+++ components/Footer.js
@@ -0,0 +1,14 @@
+const footerStyle = {
+    width: "100%",
+    position: "absolute",
+    bottom: "0px",
+    background: "blue"
+    };
+  
+  const Footer = () => (
+    <div className="Footer" style={footerStyle}>
+      FOOTER
+    </div>
+  );
+  
+  export default Footer;
