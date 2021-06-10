cedabc257a30f3bee26214fa33b9311d203a46b8
fluentinstroll <fluentinstroll@protonmail.com>
2021-06-10 10:33:28 -0400

    fixed global css no working and added examples to projects page

--- pages/projects.js
+++ pages/projects.js
@@ -21,84 +21,56 @@ export default function Projects() {
           culpa qui officia deserunt mollit anim id est laborum.
         </p>
       </div>
-      <div className={styles.projectBox}>
+      <div className={styles.projectsContainer}>
+        <div className={styles.projectBox}>
           <h1>Project 1</h1>
-         <h4><p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do<br/>
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim<br/>
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut<br/>
-            aliquip ex ea commodo consequat. Duis aute irure dolor in<br/>
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla<br/>
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in<br/>
-            culpa qui officia deserunt mollit anim id est laborum.<br/>
-          </p>
-          </h4> 
-        </div>
-        <div className={styles.projectBox}>
-          <h1>Project 2</h1>
-         <h4><p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do<br/>
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim<br/>
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut<br/>
-            aliquip ex ea commodo consequat. Duis aute irure dolor in<br/>
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla<br/>
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in<br/>
-            culpa qui officia deserunt mollit anim id est laborum.<br/>
-          </p>
-          </h4> 
-        </div>
-        <div className={styles.projectBox}>
-          <h1>Project 3</h1>
-         <h4><p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do<br/>
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim<br/>
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut<br/>
-            aliquip ex ea commodo consequat. Duis aute irure dolor in<br/>
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla<br/>
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in<br/>
-            culpa qui officia deserunt mollit anim id est laborum.<br/>
-          </p>
-          </h4> 
-        </div>
-        <div className={styles.projectBox}>
-          <h1>Project 4</h1>
-         <h4><p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do<br/>
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim<br/>
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut<br/>
-            aliquip ex ea commodo consequat. Duis aute irure dolor in<br/>
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla<br/>
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in<br/>
-            culpa qui officia deserunt mollit anim id est laborum.<br/>
-          </p>
-          </h4> 
-        </div>
-        <div className={styles.projectBox}>
-          <h1>Project 5</h1>
-         <h4><p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do<br/>
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim<br/>
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut<br/>
-            aliquip ex ea commodo consequat. Duis aute irure dolor in<br/>
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla<br/>
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in<br/>
-            culpa qui officia deserunt mollit anim id est laborum.<br/>
-          </p>
-          </h4> 
-        </div>
-        <div className={styles.projectBox}>
-          <h1>Project 6</h1>
-          <h4><p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do<br/>
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim<br/>
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut<br/>
-            aliquip ex ea commodo consequat. Duis aute irure dolor in<br/>
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla<br/>
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in<br/>
-            culpa qui officia deserunt mollit anim id est laborum.<br/>
-          </p>
+          <h4>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
           </h4>
         </div>
+        <div className={styles.projectBox}>
+          <h1 className={styles.projectText}>Project 2</h1>
+          <h4 className={styles.projectText}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </h4>
+        </div>
+        <div className={styles.projectBox2}>
+          <h1>Project 3</h1>
+          <h4>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </h4>
+        </div>
+        <div className={styles.projectBox2}>
+          <h1 className={styles.projectText}>Project 4</h1>
+          <h4 className={styles.projectText}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </h4>
+        </div>
+      </div>
     </Layout>
   );
 }
