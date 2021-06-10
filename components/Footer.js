cfcae3429ed1d9c11a68cf2116d284e41c0381d4
fluentinstroll <fluentinstroll@protonmail.com>
2021-06-10 10:49:38 -0400

    added a proper footer with boilerplate info

--- components/Footer.js
+++ components/Footer.js
@@ -2,12 +2,16 @@ const footerStyle = {
     width: "100%",
     position: "static",
     bottom: "0px",
-    background: "blue"
+    background: "#21D19F",
+    padding: "20px"
     };
   
   const Footer = () => (
     <div className="Footer" style={footerStyle}>
-      FOOTER
+      <p>Address line 1</p>
+      <p>Address Line 2</p>
+      <p>Address Line 3</p>
+      <p>Address Line 4</p>
     </div>
   );
   
