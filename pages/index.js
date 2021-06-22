823623ceb882ce4bbf2611d0ff5b18d31419ff17
fluentinstroll <fluentinstroll@protonmail.com>
2021-06-22 11:22:09 -0400

    centered some text to see how it look

--- pages/index.js
+++ pages/index.js
@@ -36,20 +36,26 @@ export default function Home() {
               <Carousel.Caption className={styles.carouselCaption}>
                 <h2>Quality you can see</h2>
                 <p>Perfectly crafted and fine tuned to your needs</p>
-                <Button href="/about" className={styles.frontpageButton}>Learn More</Button>
+                <Button href="/about" className={styles.frontpageButton}>
+                  Learn More
+                </Button>
               </Carousel.Caption>
             </Carousel.Item>
           </Carousel>
-
-          <h1 className={styles.homeHeader}>For a service you can trust</h1>
-
-          <p className={styles.homeInfo}>
-            Count on us to bring the capacity and agility for any job, big or
-            small. Our expert team is committed to the highest standards and
-            when challenges come up, we find solutions. Simple as that.
-          </p>
-          <Button href="/why-us" className={styles.frontpageButton}>Learn More</Button>
-          <h1 className={styles.homeHeader}>Satisfaction guaranteed</h1>
+          
+            <h1 className={styles.homeHeader}>For a service you can trust</h1>
+            <div className={styles.frontpageTextAlign}>
+            <p className={styles.homeInfo}>
+              Count on us to bring the capacity and agility for any job, big or
+              small. Our expert team is committed to the highest standards and
+              when challenges come up, we find solutions. Simple as that.
+            </p>
+            <Button href="/why-us" className={styles.frontpageButton}>
+              Learn More
+            </Button>
+          </div>
+            <h1 className={styles.homeHeader}>Satisfaction guaranteed</h1>
+          
           <div className={styles.projectsSection}>
             <div>
               <p className={styles.projectsInfo}>
@@ -57,22 +63,43 @@ export default function Home() {
                 extremely satisfied clients. Take a look at our portfolio of
                 work.
               </p>
-              <Button href="/projects" className={styles.frontpageButton}>Our projects</Button>
+              <Button href="/projects" className={styles.frontpageButton}>
+                Our projects
+              </Button>
             </div>
             <Image src="/assets/homePage_1.jpg" className={styles.homeImage} />
           </div>
-          <h1 className={styles.homeHeader}>Want an opinion? Contact us today</h1>
+            <h1 className={styles.homeHeader}>
+              Want an opinion? Contact us today
+            </h1>
           <div className={styles.contactSection}>
             <div className={styles.contactSectionPictures}>
-              <Image src="/assets/homeContact_1.jpeg" className={styles.contactImage}/>
-              <Image src="/assets/homeContact_2.jpg" className={styles.contactImage}/>
-              <Image src="/assets/homeContact_3.jpg" className={styles.contactImage}/>
-              <Image src="/assets/homeContact_4.jpeg" className={styles.contactImage}/>
+              <Image
+                src="/assets/homeContact_1.jpeg"
+                className={styles.contactImage}
+              />
+              <Image
+                src="/assets/homeContact_2.jpg"
+                className={styles.contactImage}
+              />
+              <Image
+                src="/assets/homeContact_3.jpg"
+                className={styles.contactImage}
+              />
+              <Image
+                src="/assets/homeContact_4.jpeg"
+                className={styles.contactImage}
+              />
             </div>
-            <p className={styles.contactInfo}>
-              Contact us through email or phone to get started with our services.
-            </p>
-            <Button href="/contact" className={styles.frontpageButton}>Contact info</Button>
+            <div className={styles.frontpageTextAlign}>
+              <p className={styles.contactInfo}>
+                Contact us through email or phone to get started with our
+                services.
+              </p>
+              <Button href="/contact" className={styles.frontpageButton}>
+                Contact info
+              </Button>
+            </div>
           </div>
         </Layout>
       </main>
