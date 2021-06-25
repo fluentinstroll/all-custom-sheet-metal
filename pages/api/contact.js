c01b17faa8be69847aaa51529c4f70385d7a5668
fluentinstroll <fluentinstroll@protonmail.com>
2021-06-25 17:22:37 -0400

    built a contact us form that has email functionality through the api/contact file

---
+++ pages/api/contact.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+
+export default function (req, res) {
+  const password = process.env.password;
+
+  let nodemailer = require('nodemailer');
+  const transporter = nodemailer.createTransport({
+    port: 465,
+    host: 'smtp.gmail.com',
+    auth: {
+      user: 'allcustomsheetmetalmailbot@gmail.com',
+      pass: password,
+    },
+    secure: true,
+  });
+
+  const mailData = {
+    from: 'allcustomsheetmetalmailbot@gmail.com',
+    to: 'fluentinstroll@protonmail.com',
+    subject: `Message From ${req.body.name}`,
+    text: req.body.message + ' | sent from: ' + req.body.email,
+    html: `<div>${req.body.message}</div><p>Sent from: ${req.body.email}</p>`,
+  };
+
+  transporter.sendMail(mailData, function (err, info) {
+    if (err) console.log(err);
+    else console.log(info);
+  });
+
+  res.status(200);
+}
--- pages/contact.js
+++ pages/contact.js
@@ -1,108 +1,85 @@
-import Layout from "../components/Layout";
-import style from "../styles/Contact.module.css";
-import {useForm} from 'react-hook-form';
+import Layout from '../components/Layout';
+import styles from '../styles/Contact.module.css';
+import { useState } from 'react';
 
 export default function Contact() {
-  
-  const {register, errors, handleSubmit, reset} = useForm();
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [submitted, setSubmitted] = useState(false);
 
-  const onSubmit = async (data) => {
-    console.log('Name: ', data.name);
-    console.log('Email: ', data.email);
-    console.log('Subject: ', data.subject);
-    console.log('Message: ', data.message);
-  }
-  
-  
-  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Sending');
+    let data = {
+      name,
+      email,
+      message,
+    };
+    fetch('/api/contact', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json, text/plain, */*',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    }).then((res) => {
+      console.log('Response received');
+      if (res.status === 200) {
+        console.log('Response succeeded!');
+        setSubmitted(true);
+        setName('');
+        setEmail('');
+        setMessage('');
+      }
+    });
+  };
+
   return (
     <Layout>
-      {/* css in js for testing */}
-    <div style={{ marginTop: "10%" }}className='ContactForm'>
-    <div className='container'>
-        <div className='row'>
-          <div className='col-12 text-center'>
-            <div className='contactForm'>
-              <form id='contact-form' onSubmit={handleSubmit(onSubmit)} noValidate>
-                {/* Row 1 of form */}
-                <div className='row formRow'>
-                  <div className='col-6'>
-                    <input
-                      type='text'
-                      name='name'
-                      {...register("name", {
-                        required: { value: true, message: 'Please enter your name' },
-                        maxLength: {
-                          value: 30,
-                          message: 'Please use 30 characters or less'
-                        }
-                      })}
-                      className='form-control formInput'
-                      placeholder='Name'
-                    ></input>
-                    {errors.name && <span className='errorMessage'>{errors.name.message}</span>}
-                  </div>
-                  <div className='col-6'>
-                    <input
-                      type='email'
-                      name='email'
-                      {...register("email", {
-                        required: true,
-                        pattern: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-                      })}
-                      className='form-control formInput'
-                      placeholder='Email address'
-                    ></input>
-                    {errors.email && (
-                      <span className='errorMessage'>Please enter a valid email address</span>
-                    )}
-                  </div>
-                </div>
-                {/* Row 2 of form */}
-                <div className='row formRow'>
-                  <div className='col'>
-                    <input
-                      type='text'
-                      name='subject'
-                      {...register("subject",{
-                        required: { value: true, message: 'Please enter a subject' },
-                        maxLength: {
-                          value: 75,
-                          message: 'Subject cannot exceed 75 characters'
-                        }
-                      })}
-                      className='form-control formInput'
-                      placeholder='Subject'
-                    ></input>
-                    {errors.subject && (
-                      <span className='errorMessage'>{errors.subject.message}</span>
-                    )}
-                  </div>
-                </div>
-                {/* Row 3 of form */}
-                <div className='row formRow'>
-                  <div className='col'>
-                    <textarea
-                      rows={3}
-                      name='message'
-                      {...register("message",{
-                        required: true
-                      })}
-                      className='form-control formInput'
-                      placeholder='Message'
-                    ></textarea>
-                    {errors.message && <span className='errorMessage'>Please enter a message</span>}
-                  </div>
-                </div>
-                <button className='submit-btn' type='submit'>
-                  Submit
-                </button>
-              </form>
-            </div>
-          </div>
-        </div>
+      <div className={styles.contactHeader}>
+        <form>
+          <formGroup className={styles.inputGroup}>
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+              name="name"
+              className={styles.inputField}
+            />
+          </formGroup>
+          <formGroup className={styles.inputGroup}>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              name="email"
+              className={styles.inputField}
+            />
+          </formGroup>
+          <formGroup className={styles.inputGroup}>
+            <label htmlFor="message">Message</label>
+            <input
+              type="text"
+              onChange={(e) => {
+                setMessage(e.target.value);
+              }}
+              name="message"
+              className={styles.inputField}
+            />
+          </formGroup>
+          <input
+            type="submit"
+            onClick={(e) => {
+              handleSubmit(e);
+            }}
+          />
+        </form>
       </div>
-    </div>
     </Layout>
   );
 }
